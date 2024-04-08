c656c225e6ce4868f7dc26aeb4860f23b73e8e63
alamjay <jayalam.97@gmail.com>
2024-04-08 12:37:16 +0100

    Revert "React context"

--- src/components/Status.js
+++
@@ -1,9 +0,0 @@
-function Status ({status}) {
-    return (
-        <div className="status">
-            <h3 className="text-3xl font-semibold">{status}</h3>
-        </div>
-    )
-}
-
-export default Status;
